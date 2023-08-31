9bfb30dd1faf5563e9451a3dd3985778a4202334
abhijittupe <136335995+abhijittupe@users.noreply.github.com>
2023-08-31 14:29:13 +0530

    abhijit files

---
+++ BookingRepository.java
@@ -0,0 +1,6 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.bookmyflight.entity.Booking;
+
+public interface BookingRepository extends JpaRepository<Booking, Integer>{
+
+}
